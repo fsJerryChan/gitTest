655078d7f98baf386568c8b7be184bd9b0d99608
fsJerryChan <190407488@qq.com>
2016-12-30 00:11:54 +0800

    jerry edit it

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,6 +1,6 @@
 
 public class TestMain {
 	public static void main(String[] args) {
-		System.out.println("Hello, this is my git test project");
+		System.out.println("Hello, this is my git test project!");
 	}
 }
